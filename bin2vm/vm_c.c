40dc1d4566e5635673df3f1af560790e2be6a608
Antoine Planchot <antoine.planchot@ensta-bretagne.org>
2016-04-07 10:44:09 +0200

    Fonction conversion 'bin2dec'.

--- bin2vm/vm_c.c
+++ bin2vm/vm_c.c
@@ -5,63 +5,82 @@
 			   1 bit   - flag o (1 : reg ; 0 : imm)
 			   16 bits - parametre o
 			   5 bits  - parametre registre 2
-			   
+
 	jmp		 : 5 bits  - fonction (01111)
+			   5 bits  - '00000'
 			   1 bits  - flag o
 			   16 bits - parametre o
 			   5 bits  - parametre registre
+
 
 	Auteur : Antoine Planchot
 */
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #define NUM_REGS 32
 
 // extraire proprement des morceaux de chaine de caractere (Python - str[a:b])
 char * extstr (char * str, int a, int b) {
-	
+
 	if (a > b || a < 0 || b >= sizeof(str) / sizeof(char)) {
 		printf("Erreur de bornes.\n");
 		exit(0);
 	}
-	
+
 	char * rslt;
 	rslt = malloc(sizeof(char) * (b-a+1));
 	int i;
-	
+
 	for (i = a; i < b; i++) {
 		rslt[i-a] = str[i];
 	}
-	
+
 	return rslt;
 }
 
 // convertit un nombre binaire ('1001001110') en décimal
 int bin2dec(char * bin) {
-	return 0;
+	int val = 1;
+	int tot = 0;
+	int i;
+
+	for (i = strlen(bin)-1; i > 0; i--) {
+		tot += val * bin[i];
+		val *= 2;
+	}
+
+	return tot;
 }
 
+// interprète une ligne d'instruction en binaire
 void inter (char * inst, char * fct, int * r1, int * r2, long * o) {
 	// test de coherence
-	if ((sizeof(inst) / sizeof(char) != 32 && strcmp(extstr(inst, 0, 5), "01111") != 1) || (sizeof(inst) / sizeof(char) != 27 && strcmp(extstr(inst, 0, 5), "01111") == 1) {
-		System.out.println("Instruction corrompue.");
+	if (strlen(inst) != 32) {
+		printf("Instruction corrompue.");
 		exit(0);
+	} else {
+		printf("Instruction correcte.");
 	}
-	
-	*fct = extstr(inst, 0, 5);
-	
-	 
+
+	// *fct = extstr(inst, 0, 5);
+
+
 }
-	
+
 
 int main (int argc, char * argv[]) {
-	
+
 	int regs[NUM_REGS];
 	FILE * code = NULL;
 
-	code = fopen("bin.txt", "r");
-	
+	code = fopen("./../data/bin.txt", "r");
+
+	char * test = "101110";
+
+	printf("%s -> %i\n", test, bin2dec(test));
+
 	return 0;
 }
