f5b8f99dbbe658cacdb06273e4320474af94bca4
Antoine Planchot <antoine.planchot@ensta-bretagne.org>
2016-04-09 10:03:08 +0200

    Point-virgule manquant...

--- bin2vm/vm_c.h
+++ bin2vm/vm_c.h
@@ -1,3 +1,3 @@
 char * extstr (char * str, int a, int b);
 int bin2dec(char * bin);
-void inter (char * inst, char * fct, int * r1, int * r2, char flag, long * o)
+void inter (char * inst, char * fct, int * r1, int * r2, int * flag, long * o);
