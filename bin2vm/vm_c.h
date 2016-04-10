d6414fcfbfb2f2c73e04bce24fc569087d9daf45
Antoine Planchot <antoine.planchot@ensta-bretagne.org>
2016-04-10 17:59:24 +0200

    Création de la structure 'cmd'.

--- bin2vm/vm_c.h
+++ bin2vm/vm_c.h
@@ -1,3 +1,11 @@
 char * extstr (char * str, int a, int b);
 int bin2dec(char * bin);
 void inter (char * inst, char * fct, int * r1, int * r2, int * flag, long * o);
+
+struct cmd {
+	char fct[5];
+	int r1;
+	int r2;
+	int flag;
+	long o;
+}
